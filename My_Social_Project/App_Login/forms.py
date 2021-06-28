1cea98f0ed7dab2c3250e233004e735a9e55c70b
Nibir <mz.nibir@gmail.com>
2021-06-28 23:03:16 +0600

    create view for sign_up (userCreation)

--- My_Social_Project/App_Login/forms.py
+++ My_Social_Project/App_Login/forms.py
@@ -2,7 +2,7 @@ from django import froms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class UserNewUser(UserCreationForm):
+class CreateNewUser(UserCreationForm):
     email = forms.EmailField(required=True)
      class Meta:
         model = User
