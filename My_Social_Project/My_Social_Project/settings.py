78d1bcc4c2f9d7682ecaead9a6e0757e6baaceec
Nibir <mz.nibir@gmail.com>
2021-06-28 23:06:53 +0600

    added crispy forms

--- My_Social_Project/My_Social_Project/settings.py
+++ My_Social_Project/My_Social_Project/settings.py
@@ -29,7 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'App_Login',
+    'crispy_forms',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
