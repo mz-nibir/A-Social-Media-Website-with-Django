6741a8275493acd642ce9dff468815c88b82a604
Nibir <mz.nibir@gmail.com>
2021-06-30 17:50:46 +0600

    create home.html and its view define

--- My_Social_Project/App_Posts/views.py
+++ My_Social_Project/App_Posts/views.py
@@ -4,6 +4,6 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
+@login_required
 def home(request):
-    return HttpResponse('Homepage')
+    return render(request, 'App_Posts/home.html', context={'title':'Home'})
