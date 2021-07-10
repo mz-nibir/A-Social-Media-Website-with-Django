00ccacb97da4b9706018a0af5d52e24d6b803bb4
Nibir <mz.nibir@gmail.com>
2021-07-10 23:06:50 +0600

    search bar add in the navbar and its view define

--- My_Social_Project/App_Posts/views.py
+++ My_Social_Project/App_Posts/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from App_Login.models import UserProfile
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 @login_required
 def home(request):
-    return render(request, 'App_Posts/home.html', context={'title':'Home'})
+    if request.method == 'GET':
+        # je keyword ti likhe search kora hobe ta collect korbo..(name=search)
+        search = request.GET.get('search', '')
+        # __icontains--> pertially match holeo colbe..and case insensitive jodi contains ar age i dei
+        result = User.objects.filter(username__icontains=search)
+
+    return render(request, 'App_Posts/home.html', context={'title':'Home', 'search':search, 'result':result})
