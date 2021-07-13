263138024c08a76d953a2181cc342b3742158778
Nibir <mz.nibir@gmail.com>
2021-07-13 15:45:36 +0600

    Like and Unlike added

--- My_Social_Project/App_Posts/views.py
+++ My_Social_Project/App_Posts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 from django.contrib.auth.decorators import login_required
 
 from App_Login.models import UserProfile, Follow
 from django.contrib.auth.models import User
-from App_Posts.models import Post
+from App_Posts.models import Post, Like
 
 # Create your views here.
 
@@ -14,10 +16,29 @@ def home(request):
     following_list = Follow.objects.filter(follower=request.user)
     # jade k follow korsi tader post gula ashbe
     posts = Post.objects.filter(author__in=following_list.values_list('following'))
+    # ei user kono post a like korese kina?
+    liked_post = Like.objects.filter(user=request.user)
+    liked_post_list = liked_post.values_list('post', flat=True)
     if request.method == 'GET':
         # je keyword ti likhe search kora hobe ta collect korbo..(name=search)
         search = request.GET.get('search', '')
         # __icontains--> pertially match holeo colbe..and case insensitive jodi contains ar age i dei
         result = User.objects.filter(username__icontains=search)
 
-    return render(request, 'App_Posts/home.html', context={'title':'Home', 'search':search, 'result':result, 'posts':posts})
+    return render(request, 'App_Posts/home.html', context={'title':'Home', 'search':search, 'result':result, 'posts':posts, 'liked_post_list':liked_post_list})
+
+@login_required
+def liked(request, pk):
+    post = Post.objects.get(pk=pk)
+    already_liked = Like.objects.filter(post=post, user=request.user)
+    if not already_liked:
+        liked_post = Like(post=post, user=request.user)
+        liked_post.save()
+    return HttpResponseRedirect(reverse('home'))
+
+@login_required
+def unliked(request, pk):
+    post = Post.objects.get(pk=pk)
+    already_liked = Like.objects.filter(post=post, user=request.user)
+    already_liked.delete()
+    return HttpResponseRedirect(reverse('home'))
